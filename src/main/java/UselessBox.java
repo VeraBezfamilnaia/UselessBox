3ea71bc3ebc61fad3c64778e3a87d80657ed225e
VeraLizunova <vera.lizunova.98@mail.ru>
2022-05-26 19:28:47 +0300

    added UselessBox

---
+++ src/main/java/UselessBox.java
@@ -0,0 +1,11 @@
+public class UselessBox {
+    private final Tumbler tumbler = new Tumbler();
+
+    public void switchOnTumbler() {
+        tumbler.switchOn();
+    }
+
+    public void switchOffTumbler() {
+        tumbler.switchOff();
+    }
+}
